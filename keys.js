cb1f938bfc27d7bf71966913bd0f9a1c662d3767
Chelsey Dolan <chelseymarie6@gmail.com>
2017-10-02 19:05:28 -0700

    another commit now with npm init

---
+++ keys.js
@@ -0,0 +1,24 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'dEyJb5Mwo3ebFOwxgI9oqmKZx',
+  consumer_secret: '21AAB50c57NeeQsssYjdUeOfjG1U4aiBxU7bLA7yikugp30dcs',
+  access_token_key: '915023746695434240-Z7YmGzx3oLFaActLD0KO8SL4F84dIYK',
+  access_token_secret: 'wY5BSLD3EZ6v3R8HcgnHxMlazbZYTR6SKg5ChdrUdEc5W',
+};
+
+var spotifyKeys = {
+    client_ID: 'a888955e9bd64e43bf3b966436fd436e',
+    client_Secret: '4b703da54a3a42dcae0692ff271b9f4f'
+};
+
+
+// var omdbKey = {
+//     apiKey: "40e9cece"
+// };
+
+module.exports = {
+    twitterKeys: twitterKeys,
+    spotifyKeys: spotifyKeys,
+    //omdbKey: omdbKey
+};
---
+++ liri.js
@@ -0,0 +1,19 @@
+// Make it so liri.js can take in one of the following commands:
+
+//    * `my-tweets`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
+
+var keys = require("./keys.js");
+var twitterObject = require(keys.twitterKeys);
+var spotifyObject = require(keys.spotifyKeys);
+
+var tweets
+
+function getTweet() {
+    var tweets = 
+}
