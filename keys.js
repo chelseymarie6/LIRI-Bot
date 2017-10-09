1ad46be797a972dbf96805ab3a93ae5d916882a4
Chelsey Dolan <chelseymarie6@gmail.com>
2017-10-08 17:35:43 -0700

    Twitter is workinggit add .

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'dEyJb5Mwo3ebFOwxgI9oqmKZx',
-  consumer_secret: '21AAB50c57NeeQsssYjdUeOfjG1U4aiBxU7bLA7yikugp30dcs',
-  access_token_key: '915023746695434240-Z7YmGzx3oLFaActLD0KO8SL4F84dIYK',
-  access_token_secret: 'wY5BSLD3EZ6v3R8HcgnHxMlazbZYTR6SKg5ChdrUdEc5W'
+    consumer_key: 'dEyJb5Mwo3ebFOwxgI9oqmKZx',
+    consumer_secret: '21AAB50c57NeeQsssYjdUeOfjG1U4aiBxU7bLA7yikugp30dcs',
+    access_token_key: '915023746695434240-Z7YmGzx3oLFaActLD0KO8SL4F84dIYK',
+    access_token_secret: 'wY5BSLD3EZ6v3R8HcgnHxMlazbZYTR6SKg5ChdrUdEc5W'
 };
 
 var spotifyKeys = {
@@ -12,13 +12,12 @@ var spotifyKeys = {
     client_Secret: '4b703da54a3a42dcae0692ff271b9f4f'
 };
 
-
-// var omdbKey = {
-//     apiKey: "40e9cece"
-// };
+var omdbKey = {
+    apiKey: "40e9cece"
+};
 
 module.exports = {
     twitterKeys: twitterKeys,
     spotifyKeys: spotifyKeys,
-    //omdbKey: omdbKey
+    omdbKey: omdbKey
 };
--- liri.js
+++ liri.js
@@ -1,20 +1,50 @@
-// Make it so liri.js can take in one of the following commands:
+var fs = require("fs");
+var argOne = process.argv[2];
+var argTwo = process.argv[3];
+var request = require("request");
+//var keysFile = require("./keys.js");
 
-//    * `my-tweets`
+//TWITTER
+var Twitter = require("twitter");
+var client = new Twitter({
+    consumer_key: 'dEyJb5Mwo3ebFOwxgI9oqmKZx',
+    consumer_secret: '21AAB50c57NeeQsssYjdUeOfjG1U4aiBxU7bLA7yikugp30dcs',
+    access_token_key: '915023746695434240-Z7YmGzx3oLFaActLD0KO8SL4F84dIYK',
+    access_token_secret: 'wY5BSLD3EZ6v3R8HcgnHxMlazbZYTR6SKg5ChdrUdEc5W'
+});
 
-//    * `spotify-this-song`
+var params = {screen_name:"CoderChelsaroo", count: 20};
 
-//    * `movie-this`
+function grabTweets(){
+    client.get("statuses/user_timeline", params, function(error, tweets, response){
+        if(error){
+            console.log(error);
+            return;
+        }
 
-//    * `do-what-it-says`
-
-var keysFile = require("./keys.js");
-var twitterObject = require(keys.twitterKeys);
-var spotifyObject = require(keys.spotifyKeys); 
-//the above will be require("node-spotify-api");
-
-var tweets
-
-function getTweet() {
-    var tweets = 
+        for (var i =0; i < tweets.length; i++){
+            console.log("Tweet # " + (i + 1) + " created on: " + tweets[i].created_at);
+            console.log("My tweet: " + tweets[i].text);
+        }
+    });
 }
+
+grabTweets();
+
+
+//OMDB
+// var omdbObject = require("request");
+
+
+
+
+
+// //SPOTIFY
+// var spotifyObject = require("node-spotify-api"); 
+// var spotifyClient = new spotifyObject("keys.spotifyKeys");
+
+// //COMMANDS
+// if(command === "my-tweets"){
+//     console.log("Calling Twitter...")
+//     grabTweets();
+// }
