c46918a50437b0c2e18fc50d60912aecd2ef9926
Chelsey Dolan <chelseymarie6@gmail.com>
2017-10-13 21:17:00 -0700

    clean, working

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var Spotify = require("node-spotify-api");//SPOTIFY
 
 //KEYS
 var client = new Twitter(keys.twitterKeys);
-var spotify = new Spotify(keys.spotifyKeys);
+var spotifyClient = new Spotify(keys.spotifyKeys);
 
 //USER INPUTS
 var argOne = process.argv[2];
@@ -30,20 +30,38 @@ var parameters = {
 var queryURL = "http://www.omdbapi.com/?t=" + argTwo + "&y=&plot=short&apikey=40e9cece";
 var rottenTomatoesURL = "https://www.rottentomatoes.com/search/?search=" + argTwo;
 
-//USER INPUT COMMANDS (trying my hand at switch/case for simplicity...)
-switch(argOne) {
-    case "my-tweets":
-        grabTweets();
-        break;
-    case "spotify-this-song":
+//USER INPUT COMMANDS 
+if (argOne === "my-tweets"){
+    grabTweets();
+}
+else if (argOne === "spotify-this-song"){
+    grabSpotify();
+}
+else if (argOne === "movie-this"){
+    grabMovie();
+}
+else if (argOne === "do-what-it-says"){
+    fs.readFile("random.txt","UTF-8", function(err, data) {
         grabSpotify();
-        break;
-    case "movie-this":
-        grabMovie();
-        break;
-    case "do-what-it-says":
-        getRandom();
-        break;
+     });
+ }
+
+function grabSpotify() {
+    var queryInput = "Never Gonna Give You Up";
+    if (argTwo !== undefined) {
+         queryInput = argTwo;
+    }
+    spotifyClient.search({ type: 'track', query: queryInput}, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " + data.tracks.items[0].album.name  + "\n" + "=================================================================");
+    });
 }
 
 //TWITTER
@@ -93,91 +111,13 @@ function grabMovie(){
             }); 
         }
 
-//SPOTIFY
-var spotifyClient = new Spotify(keys.spotifyKeys);
-function grabSpotify(){
-   // if (parameters.spotifyParam.que === undefined){
-   //     parameters.spotifyParam.query = "Never Gonna Give You Up";
-   // }
-   
-   spotifyClient.search({ type: 'track', query: "help", limit: 1}, function(err, data) {
-      //  if (argTwo){
-      //      argTwo = process.argv.splice(3).join(" "); //this splices, and joins everything after "spotify'this-song" so the function can handle songs with more than one word
-      //      query = argTwo;
-      //      console.log("Artist: " + data.tracks.items[0].artists[0].name);
-      //      console.log("Album: " + data.tracks.items[0].album.name);
-      //      console.log("Song: " + data.tracks.items[0].name);
-      //      console.log("Preview url: "+ data.tracks.items[0].href);
-        if (err) {
-         return console.log('Error occurred: ' + err);
-       } else {
-           //logs Rick Astley's Never Gonna Give You Up if no input from user
-           console.log("Artist: " + data.tracks.items[0].artists[0].name);
-           console.log("Album: " + data.tracks.items[0].album.name);
-           console.log("Song: " + data.tracks.items[0].name);
-           console.log("Preview url: "+ data.tracks.items[0].href);
-       }     
-   });
-}
-grabSpotify();
-
-
-//SPOTIFY old
-function grabSpotify(argTwo){
-    if (parameters.spotifyParam.query === undefined){
-        parameters.spotifyParam.query = "Never Gonna Give You Up";
-    }
-    
-    spotifyClient.search({type: "track", query: "Never Gonna Give You Up", limit: 1}, function(err, data) {
-        if (argTwo){
-            argTwo = process.argv.splice(3).join(" "); //this splices, and joins everything after "spotify'this-song" so the function can handle songs with more than one word
-            query = argTwo;
-
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            console.log("Album: " + data.tracks.items[0].album.name);
-            console.log("Song: " + data.tracks.items[0].name);
-            console.log("Preview url: "+ data.tracks.items[0].preview_url);
-        } else if (err) {
-          return console.log('Error occurred: ' + err);
-        } else {
-            //logs Rick Astley's Never Gonna Give You Up if no input from user
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            console.log("Album: " + data.tracks.items[0].album.name);
-            console.log("Song: " + data.tracks.items[0].name);
-            console.log("Preview url: "+ data.tracks.items[0].preview_url);
-        }     
-    });
-}
-
-//SPOTIFY test
-// var Spotify = require('node-spotify-api');
-// var spotifyClient = new Spotify(keys.spotifyKeys);
-// //SPOTIFY
-// function grabSpotify(argTwo){
-//     if (argTwo === undefined || argTwo === null){
-//         argTwo === "Never Gonne Give You Up";
-//     }
-//     spotifyClient.search({type: "track", query: argTwo, limit: 1}, function (err, data){
-//         if (err){
-//             return console.log("Error occured: " + err);
-//         }
-//         console.log("Artist: " + data.tracks.items[0].artists[0].name);
-//         console.log("Album: " + data.tracks.items[0].album.name);
-//         console.log("Song: " + data.tracks.items[0].name);
-//         console.log("Preview url: "+ data.tracks.items[0].preview_url);
-//     });
-
-// }
-//grabSpotify();
-//console.log (Spotify);
-
 //RANDOM
 function getRandom(){
     fs.readFile("random.txt","UTF-8", function(error, data) {
         if(error) {
             console.log("Something went wrong" + error);
           }
-          grabSpotify();
+          grabTweets();
       });
 };
 }
